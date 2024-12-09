dc3b2c58a85a93f073b615bbc4ce6ba47ec65a5f
Mohsin Sheikh <160018515+DevMohsinSheikh-12@users.noreply.github.com>
2024-12-09 18:40:58 +0500

    Update Head.tsx

--- src/app/components/Head.tsx
+++ src/app/components/Head.tsx
@@ -12,10 +12,10 @@ function Head() {
       >
         <div className="absolute inset-0 bg-black opacity-40"></div>
         <div className="relative z-10 text-center">
-          <h1 className="text-4xl font-bold">Blog List</h1>
+          <h1 className="text-4xl font-bold">Authentication</h1>
           <p className="text-sm mt-2">
             <span className="text-gray-400">Home </span>
-            <span className="text-orange-500">&gt; Blog</span>
+            <span className="text-orange-500">&gt; AUTH</span>
           </p>
         </div>
       </section>
